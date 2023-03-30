22f962415e893452673b3c092c22a14451e9a70a
msmilkshake <prnfilipe@gmail.com>
2023-03-30 16:12:02 +0100

    added printBoard function which can represent a baord between 1 and 99

--- src/main/scala/io/IOUtils.scala
+++ src/main/scala/io/IOUtils.scala
@@ -1,6 +1,6 @@
 package io
 
-import logic.Cells.{Blue, Board, Red}
+import logic.Cells.{Blue, Board, Cell, Red}
 
 import scala.annotation.tailrec
 
@@ -16,49 +16,121 @@ object IOUtils {
   def redString(s: String): String = {
     f"${redColor}${s}${resetColor}"
   }
+  
+  def printBoard(board: Board): Unit = {
+    
+    @tailrec
+    def buildLabelsList(n: Int, lst: List[Int]): List[Int] = {
+      n match {
+        case 0 => lst
+        case x => buildLabelsList(x - 1, x :: lst)
+      }
+    }
+    val labels = buildLabelsList(board.length, Nil)
+    
+    printBoardHeader(labels)
+    printBoardGrid(board, board.length, labels)
+    printBoardFooter(board.length)
+  }
   def printNum(n: Int): Unit = {
     print(n)
   }
   
-  def printBoardHeader(n: Int) = {
-    val lblRowOffset = 5
-    val hexEdgeOffset = 7
+  def printBoardHeader(labels: List[Int]): Unit = {
+    val labelOffset = "     "
+    val starOffset = blueString("         *")
+    val starPattern = blueString(" * *")
+    val hexOffset = "       "
+    val hexPattern = " ╱ ╲"
     
     @tailrec
-    def printNumLabels(i: Int, n: Int): Unit = {
-      if (i <= n) {
-        print(" ".repeat(3) + i)
-        printNumLabels(i + 1, n)
-      }
-    }
-    
-    def printGridLine(i: Int, n: Int): Unit = {
-      def getBoardElement(row: Int, col: Int, board: Board): String = {
-        board(row)(col) match {
-          case Red => redString("X")
-          case Blue => blueString("O")
-          case _ => " "
+    def buildLabels(labels: List[Int], s: String): String = {
+      labels match {
+        case Nil => s
+        case x :: xs => {
+          val padding = x / 10 match {
+            case 0 => " "
+            case _ => ""
+          }
+          buildLabels(xs, f"${s}  ${padding}${x}")
         }
       }
-      
-      def printSingleCell(row: Int, col: Int, board: Board): Unit = {
-        print(f" ${getBoardElement(row, col, board)} |")
-      }
-      
-      if (i <= n) {
-        print(" ".repeat(i * 2 - 1) + f"${i}  ${redString("*")}  |")
+    }
+    
+    @tailrec
+    def buildLine(n: Int, pattern: String, s: String): String = {
+      n match {
+        case 0 => s
+        case _ => buildLine(n - 1, pattern, f"${s}${pattern}")
       }
     }
     
-    print(" ".repeat(lblRowOffset))
-    printNumLabels(1, n)
-    println()
-    println(blueString("         *" + " * *".repeat(n - 1)))
-    print(" ".repeat(hexEdgeOffset))
-    print(" ╱ ╲".repeat(n))
-    println()
-    printGridLine(1, n)
+    println(buildLabels(labels, labelOffset))
+    println(buildLine(labels.length - 1, starPattern, starOffset))
+    println(buildLine(labels.length, hexPattern, hexOffset))
     
+  }
+
+  @tailrec
+  def printBoardGrid(board: Board, len: Int, labels: List[Int]): Unit = {
+    val offset = "  "
+    
+    def buildgridLine(row: List[Cell], s: String): String = {
+      
+      @tailrec
+      def buildCell(cells: List[Cell], s: String): String = {
+        cells match {
+          case Nil => s
+          case c :: cs => {
+            val cell = c match {
+              case Red => redString("X")
+              case Blue => blueString("O")
+              case _ => " "
+            }
+            buildCell(cs, f"${s} ${cell} |")
+          }
+        }
+      }
+
+      f"${buildCell(row, s)}  ${redString("*")}"
+    }
+    
+    @tailrec
+    def buildHexSep(n: Int, s: String): String = {
+      n match {
+        case 0 => f"${s}  ${redString("*")}"
+        case _ => buildHexSep(n - 1, f"${s} / \\")
+      }
+    }
+    
+    (board, labels) match {
+      case (x :: Nil, n :: Nil) => {
+        val padding = n / 10 match {
+          case 0 => " "
+          case _ => ""
+        }
+        println(buildgridLine(x, f"${padding}${offset * (n-1)}${n}  ${redString("*")}  |"))
+        
+      }
+      case (x :: xs, n :: ns) => {
+        val padding = n / 10 match {
+          case 0 => " "
+          case _ => ""
+        }
+        println(buildgridLine(x, f"${padding}${offset * (n-1)}${n}  ${redString("*")}  |") +
+            "\n" + buildHexSep(len, f"   ${offset * n}${redString("*")}  \\"))
+        printBoardGrid(xs, len, ns)
+      }
+    }
+    
+  }
+  
+  def printBoardFooter(n: Int): Unit = {
+    val offset = "      "
+    val hexPattern = " / \\"
+    val starPattern = blueString(" * *")
+    println(f"${offset}${"  " * n}\\${hexPattern * (n - 1)} /")
+    println(f"${offset}${"  " * n}${starPattern * (n - 1)} ${blueString("*")}")
   }
 
 }
--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -1,9 +1,56 @@
 import io.IOUtils
+import io.IOUtils.{printBoard, redString}
+import logic.Cells._
 
 object Tests {
 
+  val b1: Board = List(
+    List(Empty, Empty, Empty, Empty, Empty),
+    List(Empty, Empty, Empty, Empty, Empty),
+    List(Empty, Empty, Empty, Empty, Empty),
+    List(Empty, Empty, Empty, Empty, Empty),
+    List(Empty, Empty, Empty, Empty, Empty)
+  )
+
+  val b2: Board = List(
+    List(Red, Empty, Empty, Blue, Empty),
+    List(Empty, Red, Empty, Blue, Empty),
+    List(Empty, Red, Empty, Empty, Blue),
+    List(Empty, Empty, Blue, Blue, Empty),
+    List(Empty, Empty, Empty, Red, Red)
+  )
+
+  val b3: Board = List(
+    List(Empty, Empty, Empty, Empty, Blue, Empty, Empty, Empty),
+    List(Empty, Red, Red, Blue, Empty, Empty, Empty, Empty),
+    List(Red, Empty, Blue, Red, Red, Empty, Empty, Empty),
+    List(Empty, Blue, Empty, Empty, Red, Red, Empty, Empty),
+    List(Empty, Blue, Blue, Empty, Empty, Red, Red, Empty),
+    List(Empty, Empty, Blue, Empty, Empty, Empty, Red, Red),
+    List(Empty, Blue, Empty, Empty, Empty, Empty, Empty, Empty),
+    List(Empty, Blue, Empty, Empty, Empty, Empty, Empty, Empty),
+  )
+
+  val b4: Board = List(
+    List(Blue, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,Empty, Red),
+    List(Empty, Blue, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,Red, Empty),
+    List(Empty, Empty, Blue, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Red,Empty, Empty),
+    List(Empty, Empty, Empty, Blue, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Red, Empty,Empty, Empty),
+    List(Empty, Empty, Empty, Empty, Blue, Empty, Empty, Empty, Empty, Empty, Red, Empty, Empty,Empty, Empty),
+    List(Empty, Empty, Empty, Empty, Empty, Blue, Empty, Empty, Empty, Red, Empty, Empty, Empty,Empty, Empty),
+    List(Empty, Empty, Empty, Empty, Empty, Empty, Blue, Empty, Red, Empty, Empty, Empty, Empty,Empty, Empty),
+    List(Empty, Empty, Empty, Empty, Empty, Empty, Empty, Red, Empty, Empty, Empty, Empty, Empty,Empty, Empty),
+    List(Empty, Empty, Empty, Empty, Empty, Empty, Red, Empty, Blue, Empty, Empty, Empty, Empty,Empty, Empty),
+    List(Empty, Empty, Empty, Empty, Empty, Red, Empty, Empty, Empty, Blue, Empty, Empty, Empty,Empty, Empty),
+    List(Empty, Empty, Empty, Empty, Red, Empty, Empty, Empty, Empty, Empty, Blue, Empty, Empty,Empty, Empty),
+    List(Empty, Empty, Empty, Red, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Blue, Empty,Empty, Empty),
+    List(Empty, Empty, Red, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Blue,Empty, Empty),
+    List(Empty, Red, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,Blue, Empty),
+    List(Red, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,Empty, Blue),
+  )
+  
   def main(args: Array[String]): Unit = {
-    IOUtils.printBoardHeader(5)
+    printBoard(b4)
   }
 
 }

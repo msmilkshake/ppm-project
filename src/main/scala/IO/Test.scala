302c55d57619dad446cb88d9fe2fd2fe5fd6c0ce
Tiag0L0b0 <tiagolobo22@gmail.com>
2023-04-05 18:08:08 +0100

    "testing github"

--- src/main/scala/IO/Test.scala
+++ src/main/scala/IO/Test.scala
@@ -7,8 +7,9 @@ object Test {
 
   def main(args: Array[String]): Unit = {
     def b1 = List(List(Red,Empty,Blue),List(Empty,Empty,Empty),List(Empty,Blue,Empty))
+    def b2 = List(List(Red,Empty,Blue,Empty),List(Empty,Empty,Empty,Blue),List(Empty,Blue,Empty,Red),List(Empty,Empty,Empty,Empty))
 
-    drawBoard(b1)
+    drawBoard(b2)
   }
 
 }

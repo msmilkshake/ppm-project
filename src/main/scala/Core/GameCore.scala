d25313573f7463bcac062f00fe9e392080b14e28
Tiag0L0b0 <tiagolobo22@gmail.com>
2023-04-08 11:02:13 +0100

    "play method (T2) done"

--- src/main/scala/Core/GameCore.scala
+++ src/main/scala/Core/GameCore.scala
@@ -6,10 +6,23 @@ import Core.Cells.{Cell, Empty}
 
 object GameCore {
 
+  //T1
   def randomMove (board: Board, rand: MyRandom):((Int, Int),MyRandom) = {
     val validCells = filterCells(board,Empty) //lista celulas vazias validas para jogar
     val (n, nextRnd) = rand.nextInt(validCells.length) // escolher um número entre 0 e o máx da lista (será o indice)
     (validCells(n),nextRnd.asInstanceOf[MyRandom]) //posição aleatória da lista de celulas vazias, nextRandom
+  }
+
+  //T2
+  def play(board: Board, player: Cells.Cell, row: Int, col: Int): Board = {
+    board.zipWithIndex.map{ //obter uma lista de tuplos (lista(cell),index da linha)
+      case (lstCell,rowIndex) => if (rowIndex == row) { // se o index da linha igual a row
+        lstCell.zipWithIndex.map{ //obter uma lista de tuplos (cell, index da coluna)
+          case (cell,index) => if (index == col) player else cell //se index da coluna igual a col, criar novo board com
+                                                                  // a cell na posição (row,col) igual a player
+        }
+      } else lstCell
+    }
   }
 
 
